395535719e8416ebf232403656cb18115d6c5d42
robertbruntz <44660610+robertbruntz@users.noreply.github.com>
2020-03-05 18:59:14 -0500

    Changed underscores to dashes in dqsegdbFindEndTime
    Function def for dqsegdbFindEndTime had underscores under "Parameters", but Sphinx (?) didn't like that, and the documentation for that function came out formatted slightly differently.  Changed the underscores to dashes.

--- dqsegdb/apicalls.py
+++ dqsegdb/apicalls.py
@@ -133,7 +133,7 @@ def dqsegdbFindEndTime(flag_dict):
     Determines max end_time from known times in flag_dict
 
     Parameters
-    __________
+    ----------
     flag_dict: `dictionary`
         Input dictionary, converted from json using json.loads in previous call
 
